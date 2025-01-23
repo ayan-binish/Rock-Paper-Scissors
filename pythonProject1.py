ab01439b0fd7908dfaa23682b65c3989770819b5
Ayan Ayinipulli Binish <ayaanbinish@gmail.com>
2025-01-22 18:15:57 -0800

    Add files via upload
    Rock Paper Scissors Game.

---
+++ pythonProject1.py
@@ -0,0 +1,9 @@
+from tkinter import *
+
+root = Tk()
+root.title('Rock Paper Scissors')
+root.geometry("500x600")
+# Change the bg color to white
+root.config(bg="white")
+
+rock = PhotoImage(file='images/Users/Ayan/Downloads/rock.jpg')
